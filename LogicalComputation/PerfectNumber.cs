2fffdab39fa2eae77ca697f83ad05d7e07fc9fbf
shashidharbollu <shashidharbollu77@gmail.com>
2022-04-13 16:31:03 +0530

    [shashidhar] Add : programme for perfect number has been updated

---
+++ LogicalComputation/PerfectNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalComputation
+{
+    public class PerfectNumber
+    {
+        public void perfectnumber()
+        {
+            int number, i, rem, sum = 0;
+            Console.WriteLine("enter your number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i < number; i++)
+            {
+                rem = number % i;
+                if (rem == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if (sum == number)
+            {
+                Console.WriteLine(number +"  " + "is a perfect number");
+            }
+            else
+            { 
+            Console.WriteLine(number + "  " + "is not a perfect number");
+            }
+
+
+        }
+    }
+}
--- LogicalComputation/Program.cs
+++ LogicalComputation/Program.cs
@@ -3,8 +3,31 @@ using LogicalComputation;
 class Program
 {
     static void Main(String[] args)
-    { 
-    FibonacciSeries series = new FibonacciSeries();
-        series.fibibonacciseries();
+    {
+        Console.WriteLine("welcome to my logical programmes, please chose your option :  ");
+        bool end = true;
+
+        while (end)
+        {
+            Console.WriteLine("1.fibonacciseries\n 2.perfectnumber\n end the programme. ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FibonacciSeries series = new FibonacciSeries();
+                    series.fibibonacciseries();
+                    break;
+                case 2:
+                    PerfectNumber n = new PerfectNumber();
+                    n.perfectnumber();
+                    break;
+                
+
+                default:
+                    Console.WriteLine("choose a proper number to execute");
+                    break;
+            }
+        }
     }
 }
+
