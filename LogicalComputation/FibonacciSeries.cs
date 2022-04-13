0c4d3ee4347547deee0833ab03a1c924df7385b1
shashidharbollu <shashidharbollu77@gmail.com>
2022-04-13 15:37:25 +0530

    [shashidhar] Add: programme for fibonacci series is updated

---
+++ LogicalComputation/FibonacciSeries.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalComputation
+{
+    public class FibonacciSeries
+    {
+        public void fibibonacciseries()
+        {
+            int num1 = 0, num2 = 1, sum = 0 ;
+
+            Console.WriteLine(num1+" "+num2);
+            for (int i = 2; i < 10; i++)
+            { 
+            sum = num1 + num2;
+                Console.WriteLine("  "+sum);
+                num1 = num2;
+                num2 = sum;
+            }
+
+        }
+    }
+}
---
+++ LogicalComputation/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using LogicalComputation;
+class Program
+{
+    static void Main(String[] args)
+    { 
+    FibonacciSeries series = new FibonacciSeries();
+        series.fibibonacciseries();
+    }
+}
